8866fe7a33e66caccb252e1fa9206335892abae0
michael_000 <michael_000@VIERNE>
2016-12-28 15:08:49 +0100

    Projektdateien hinzufügen.

---
+++ HelloWindowsForms/FrmMain.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace HelloWindowsForms
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxInput = new System.Windows.Forms.TextBox();
+            this.tbxOutput = new System.Windows.Forms.TextBox();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbxInput
+            // 
+            this.tbxInput.Location = new System.Drawing.Point(22, 67);
+            this.tbxInput.Name = "tbxInput";
+            this.tbxInput.Size = new System.Drawing.Size(100, 20);
+            this.tbxInput.TabIndex = 0;
+            // 
+            // tbxOutput
+            // 
+            this.tbxOutput.Location = new System.Drawing.Point(129, 66);
+            this.tbxOutput.Name = "tbxOutput";
+            this.tbxOutput.Size = new System.Drawing.Size(100, 20);
+            this.tbxOutput.TabIndex = 1;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(84, 122);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(75, 23);
+            this.btnCopy.TabIndex = 2;
+            this.btnCopy.Text = "Copy";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(252, 188);
+            this.Controls.Add(this.btnCopy);
+            this.Controls.Add(this.tbxOutput);
+            this.Controls.Add(this.tbxInput);
+            this.Name = "FrmMain";
+            this.Text = "Main Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxInput;
+        private System.Windows.Forms.TextBox tbxOutput;
+        private System.Windows.Forms.Button btnCopy;
+    }
+}
+
